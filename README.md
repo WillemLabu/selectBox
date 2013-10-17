e1a03148c8654ced3dbbdd1571303208d5d23fdd
Willem Labu <willem@gloo.co.za>
2013-10-17 10:35:32 +0200

    super basics

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ selectBox
 =========
 
 Very simple jQuery &lt;select> wrapper plugin.
+
+## Usage:
+
+### HTML
+
+    <select>
+        <option val="0">Option label</option>
+        <option val="1">Option label</option>
+        <option val="2">Option label</option>
+    </select>
+	
+### JS
+
+    (function(){
+        $('select').selectBox();
+    });
+	
+### Notes:
+
+- Add your own styles. These are the minimum.
+- Add an arrow: there's a <span class="indicator">
+- First <option> becomes the label.
---
+++ selectBox.js
@@ -0,0 +1,58 @@
+/*
+* selectBox
+* <select> wrapper for custom styles
+* By Willem Labu
+*/
+(function ($) {
+
+	$.fn.selectBox = function () {
+
+		this.each(function () {
+
+			$(this).wrap('<div class="selectBox" />');
+
+			var	a = $('<a />'),
+				b = $('<span class="label" />').html($($(this).children('option')[0]).text()),
+				c = $('<span class="indicator" />').appendTo(a);
+
+			b.appendTo(a);
+			a.appendTo($(this).parent('.selectBox'));
+
+			// Show a visible change when a selection is made
+			$(this).on('change', function () {
+
+				var options = [];
+				$(this).children('option').each(function(){
+					options.push($(this).val())
+				});
+
+				// This is horrible...
+				$(this).siblings('a').children('.label').html(
+					$(
+						$(this).children('option')[
+							$.inArray($(this).val(), options)
+						]
+					).text()
+				);
+			});
+
+			// Add a class when hovering
+			$(this).hover(
+				function () {
+					$(this).parent('.selectBox').addClass('over')
+				},
+				function () {
+					$(this).parent('.selectBox').removeClass('over')
+				}
+			);
+
+			// Remove error class when user interacts with the select
+			$(this).on('click focus', function () {
+				$(this).parent('.selectBox').removeClass('error');
+			});
+
+		});
+
+	}
+
+})(jQuery);
